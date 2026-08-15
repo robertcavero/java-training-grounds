ad4180667b1634184e622776270622aa6ae5f2a7
Robert Cavero <135251520+robertcavero@users.noreply.github.com>
2026-08-15 16:39:51 -0300

    adding from another machine

---
+++ tipos_primitivos_and_wrappers/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        for (Integer i = 0; i <100; i++){
+            Integer i = 1;
+            i++;
+
+            Integer in = new Integer(1);
+            var pi = in.intValue();
+            pi++;
+            pi = new Integer(pi);
+
+            int a = 1;
+
+
+
+        }
+
+        int[] arr = new int[]();
+        int i = null; //primitivo não aceita null
+        List<int> list = new ArrayList<>();
+        //não aceita tipo primitivo
+        //tem que usar wrapper (ex. Integer)
+
+    }
+}
