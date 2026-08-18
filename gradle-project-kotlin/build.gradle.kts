3b80b4b472ed0f31ffb7d8d728dade85a5a075e0
RobertCavero <robertcavero404@gmail.com>
2026-08-18 02:54:33 -0300

    annotations start

--- gradle-project-kotlin/build.gradle.kts
+++ gradle-project-kotlin/build.gradle.kts
@@ -1,26 +1,43 @@
 plugins {
     id("java")
+    checkstyle
 }
 
 group = "br.com.dio"
 version = "1.0-SNAPSHOT"
+
 val mapstructVersion = "1.6.3"
-var lombokVersion = "1.18.42"
+val lombokVersion = "1.18.46"
 val lombokMapstructBinding = "0.2.0"
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    // Código principal
+    compileOnly("org.projectlombok:lombok:$lombokVersion")
     implementation("org.mapstruct:mapstruct:$mapstructVersion")
-    implementation("org.projectlombok:lombok-mapstruct-binding:0.2.0")
 
-    compileOnly("org.projectlombok:lombok:1.18.46")
+    // Processadores de Anotação (A ordem exata abaixo é obrigatória)
+    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
+    annotationProcessor("org.projectlombok:lombok-mapstruct-binding:$lombokMapstructBinding")
+    annotationProcessor("org.mapstruct:mapstruct-processor:$mapstructVersion")
 
-    annotationProcessor("org.mapstruct:mapstruct:$mapstructVersion")
-    annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+    // Testes
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
 
-    annotationProcessor("org.projectlombok:lombok:1.18.46")
+tasks.withType<Checkstyle>().configureEach {
+    reports{
+        xml.required = true
+        html.required = true
+    }
+}
+
+tasks.checkstyleMain{
+    source = fileTree("src/main/java")
 }
 
 tasks.test {
---
+++ runtime_annotations/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+group = "br.com.dio"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.google.guava:guava:33.4.8-jre")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
